9b26f60af0bb48b551f4e91dfb7205526181723e
Oliver-Akins <oliver.akins1999@gmail.com>
2023-02-24 09:43:34 -0700

    Discord escape fix?

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,8 +1,8 @@
 export function escapeDiscordMarkdown(message: string) {
 	let m = message;
-	m = m.replace(/_/gi, `\_`);
-	m = m.replace(/\*/gi, `\*`);
-	m = m.replace(/~/gi, `\~`);
-	m = m.replace(/\|\|/gi, `\||`);
+	m = m.replace(/_/gi, `\\_`);
+	m = m.replace(/\*/gi, `\\*`);
+	m = m.replace(/~/gi, `\\~`);
+	m = m.replace(/\|\|/gi, `\\||`);
 	return m;
 };
